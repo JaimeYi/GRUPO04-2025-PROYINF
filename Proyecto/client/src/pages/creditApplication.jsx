2aadf961e4cba1805d3c94d5886dbb1972beaf4c
JaimeYi <jguzman@usm.cl>
2025-11-06 14:51:55 -0300

    some changes

--- Proyecto/client/src/pages/creditApplication.jsx
+++ Proyecto/client/src/pages/creditApplication.jsx
@@ -6,16 +6,37 @@ import "../css/creditApplication.css";
 
 function CreditApplication(){
     const { user, isLoading } = useAuth();
+    const [isMessageVisible, setIsMessageVisible] = useState(true);
+
+    useEffect(() => {
+        // 4. Configura el temporizador
+        const timerId = setTimeout(() => {
+            // 5. Después de 2 segundos, actualiza el estado para ocultar el mensaje
+            setIsMessageVisible(false);
+        }, 2000); // 2000ms = 2 segundos
+
+        // 6. (Opcional pero recomendado) Limpia el temporizador 
+        // si el componente se desmonta antes de tiempo.
+        return () => {
+            clearTimeout(timerId);
+        };
+        
+    }, []);
 
     if (isLoading) {
         return null;
     }
+
     return (
         <div>
             {!(user === null || user.userType === "noCliente") ? (
                 <>
                     <Navbar />
-                    
+                    {isMessageVisible && (
+                        <p id="verifyCreditText">
+                            Verificando requisitos para el préstamo...
+                        </p>
+                    )}
                 </>
             ) : (
                 <>
